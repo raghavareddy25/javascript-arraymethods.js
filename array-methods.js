bbb5b83a56280e41fc3f4f60824fa04aadeeaf59
raghavareddy <raghavareddy2325@gmail.com>
2022-08-18 00:36:21 +0530

    code added

---
+++ array-methods.js
@@ -0,0 +1,29 @@
+// push  
+var fruits=["Banana", "Orange", "Apple", "Mango"]
+fruits.push("kiwi");
+console.log(fruits);
+
+// pop
+var fruits=["Banana", "Orange", "Apple", "Mango"]
+console.log(fruits.pop());
+
+// delete
+var fruits=["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
+console.log(fruits);
+
+// shift
+var fruits=["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);
+
+// .tostring
+var fruits=["Banana", "Orange", "Apple", "Mango"]
+console.log(fruits.toString());
+
+// unshift
+var fruits=["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
+console.log(fruits);
+
+
